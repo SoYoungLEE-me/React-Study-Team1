93ef56ccf991047aba7d2506c21c15086e2907b2
SoYoungLEE-me <lsy000626@gmail.com>
2025-11-18 21:08:42 +0900

    feat: mealReport, Settings 페이지 ui 구현

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MainPage from "./pages/Main/MainPage";
 import MealRecordPage from "./pages/MealRecord/MealRecordPage";
 import MealReportPage from "./pages/MealReport/MealReportPage";
 import Login from "./pages/Login/Login";
+import SettingsPage from "./pages/SettingsPage/SettingsPage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/record" element={<MealRecordPage />} />
           <Route path="/report" element={<MealReportPage />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/settings" element={<SettingsPage />} />
         </Route>
       </Routes>
     </div>
