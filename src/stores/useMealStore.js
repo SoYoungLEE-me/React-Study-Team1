a529528c6b88388b1ba030ab4882a7e272db34c2
YunheeAhn <ayh95944674@gmail.com>
2025-11-19 16:36:13 +0900

    feat : 식단 데이터, 날짜 저장 store

--- src/stores/useMealStore.js
+++ src/stores/useMealStore.js
@@ -1,17 +1,17 @@
 import { create } from "zustand";
+import { getToday } from "../utils/getToday";
 
 export const useMealStore = create((set, get) => ({
-  meals: {}, // 식단데이터
-  selectedDate: null, // 선택 날짜
-  editType: null, // 현재 편집중인 식단(아침,점심,저녁,간식)
+  meals: {},
 
-  // 달력에서 날짜 선택
-  setSelectedDate: (date) => set({ selectedDate: new Date(date), editType: null }),
+  selectedDate: getToday(),
 
-  // 수정 할 식단(아침,점심,저녁,간식) 선택
+  editType: null,
+
+  setSelectedDate: (date) => set({ selectedDate: date, editType: null }),
+
   setEditType: (type) => set({ editType: type }),
 
-  // 식단 저장
   saveMeal: (date, type, items) => {
     const { meals } = get();
     set({
@@ -25,15 +25,14 @@ export const useMealStore = create((set, get) => ({
     });
   },
 
-  // 날짜의 식단 데이터 get
   getMealsByDate: (date) => {
     const { meals } = get();
     return (
       meals[date] || {
-        breakfast: [], // 아침데이터
-        lunch: [], // 점심데이터
-        dinner: [], // 저녁데이터
-        snack: [], // 간식데이터
+        breakfast: [],
+        lunch: [],
+        dinner: [],
+        snack: [],
       }
     );
   },
